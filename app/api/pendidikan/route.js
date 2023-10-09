7825a6a0cc5d52da2e5f8b2fa8d9bf4c02581a36
Thoriq Ibadurrohman <221810626@stis.ac.id>
2023-10-09 16:17:26 +0700

    fix route pendidikan

--- app/api/pendidikan/route.js
+++ app/api/pendidikan/route.js
@@ -1,13 +1,13 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const getAllPendidikanTerakhir = async () => {
-  const res = prisma.pendidikan.findMany();
-  return res;
-};
+// const getAllPendidikanTerakhir = async () => {
+//   const res = prisma.pendidikan.findMany();
+//   return res;
+// };
 
-async function GET() {
-  const allPendidikan = await getAllPendidikanTerakhir();
-  return NextResponse.json(allPendidikan);
-}
+// async function GET() {
+//   const allPendidikan = await getAllPendidikanTerakhir();
+//   return NextResponse.json(allPendidikan);
+// }
 
-export { GET };
+// export { GET };
